d81140d5de881fc6aa5384c2eeec1b9dfc35fbcd
Kushagra910 <106877681+Kushagra910@users.noreply.github.com>
2023-02-04 22:27:59 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      backgroundImage:{
+        'img1':"url(./images/Image0.svg)"
+      }
+    },
+  },
+  plugins: [],
+}
